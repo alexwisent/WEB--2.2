45ee73195b41ef5000d1cf66d62984ac7f77394e
Софья Анчугова <anchugova.sonya@gmail.com>
2025-05-15 22:22:38 +0700

    обновление файла с книгами

--- lab6/book.js
+++ lab6/book.js
@@ -1,17 +1,82 @@
 const books = [
     {
-        author: 'Лао-цзы', 
-        name: 'Дао дэ цзин. Канон о Дао и дэ', 
-        publicationYear: 2022
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 450,
+        bestseller: true,
+        available: true
     },
     {
         author: 'Карл Саган',
-        name: 'Космос', 
-        publicationYear: 2024
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 720,
+        bestseller: true,
+        available: false
     },
     {
-        author: 'Баладжи Касал', 
-        name: 'Ошибки разумного инвестора', 
-        publicationYear: 2025
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 530,
+        bestseller: false,
+        available: true
     },
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 390,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: 480,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Александр Пушкин',
+        name: 'Евгений Онегин',
+        publicationYear: 1833,
+        price: 350,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Рэй Брэдбери',
+        name: '451° по Фаренгейту',
+        publicationYear: 1953,
+        price: 410,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Айзек Азимов',
+        name: 'Основание',
+        publicationYear: 1951,
+        price: 630,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Юваль Ной Харари',
+        name: 'Sapiens. Краткая история человечества',
+        publicationYear: 2011,
+        price: 850,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 470,
+        bestseller: true,
+        available: false
+    }
 ];
