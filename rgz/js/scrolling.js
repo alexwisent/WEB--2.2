4514715ce071241683e78749a47dfa46df9ca2c5
Софья Анчугова <anchugova.sonya@gmail.com>
2025-06-01 04:59:51 +0700

    добавление комментариев

--- rgz/js/scrolling.js
+++ rgz/js/scrolling.js
@@ -1,4 +1,4 @@
-document.querySelector('.back-to-top').addEventListener('click', function(e) {
-	e.preventDefault();
-	window.scrollTo({top: 0, behavior: 'smooth'});
+document.querySelector('.back-to-top').addEventListener('click', function(e) { // Находим элемент на странице с классом 'back-to-top' и добавляем ему обработчик события 'click'
+	e.preventDefault(); // Отменяем стандартное поведение элемента
+	window.scrollTo({top: 0, behavior: 'smooth'}); // Плавно прокручиваем страницу к верху (к координате top: 0)
 });
