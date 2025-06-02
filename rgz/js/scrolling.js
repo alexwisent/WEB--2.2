17efaf4107b87009816c4abb4cde21ba63d7f126
Софья Анчугова <anchugova.sonya@gmail.com>
2025-06-02 17:02:03 +0700

    добавление комментариев

--- rgz/js/scrolling.js
+++ rgz/js/scrolling.js
@@ -1,3 +1,4 @@
+// Кнопка "Наверх"
 document.querySelector('.back-to-top').addEventListener('click', function(e) { // Находим элемент на странице с классом 'back-to-top' и добавляем ему обработчик события 'click'
 	e.preventDefault(); // Отменяем стандартное поведение элемента
 	window.scrollTo({top: 0, behavior: 'smooth'}); // Плавно прокручиваем страницу к верху (к координате top: 0)
